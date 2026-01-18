6905a3ea953d4f2e7978e274f3ca07effeb3c332
swapnil-railkar <swapnilrailkar01@gmail.com>
2026-01-18 20:11:00 +0530

    [engine]-added encap

--- engine/src/main/java/org/solvr/dto/RequestDto.java
+++ engine/src/main/java/org/solvr/dto/RequestDto.java
@@ -1,8 +1,19 @@
 package org.solvr.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class RequestDto {
-    public String token;
-    public String problemStatement;
-    public String language;
-    public Boolean showHints;
+    private String token;
+    private String problemStatement;
+    private String language;
+    private Boolean showHints;
 }
