86704f49e7159580ddfa6cb6af26791cd15a8d09
swapnil-railkar <swapnilrailkar01@gmail.com>
2026-02-01 20:08:21 +0530

    [ui]-added persistance

--- ui/src/store/index.js
+++ ui/src/store/index.js
@@ -1,9 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import problemReducer from "./problem";
 import solutionReducer from "./solution";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
 
-const store = configureStore({
-    reducer: {problem: problemReducer, solution: solutionReducer}
-})
+const rootReducer = combineReducers({
+  problem: problemReducer,
+  solution: solutionReducer,
+});
 
-export default store;
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["problem", "solution"],
+};
+
+const storePersistReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: storePersistReducer,
+});
+
+export const persistor = persistStore(store);
